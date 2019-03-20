0ece66abfd98f7e654f94f80b63760a83f275451
Chen Engel <Henengel@gmail.com>
2019-03-20 15:52:58 -0400

    Add movies service
    # Create service interface
    # Refactor URL's

--- app/src/main/java/com/example/popularmovies/api/main/MoviesService.kt
+++ app/src/main/java/com/example/popularmovies/api/main/MoviesService.kt
@@ -4,10 +4,25 @@ import com.example.popularmovies.api.main.models.ResponseMoviesList
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
+
+private const val PATH_ENDPOINT = "endpoint"
+private const val PATH_CATEGORY = "category"
+
+private const val QUERY_API_KEY = "api_key"
+private const val QUERY_LANGUAGE = "language"
+private const val QUERY_PAGE = "page"
 
 interface MoviesService {
 
-    @GET("{endpoint}")
-    fun getMovies(@Path("endpoint") endpoint: String): Call<ResponseMoviesList>
+    @GET("{$PATH_ENDPOINT}/{$PATH_CATEGORY}")
+    fun getMovies(
+        @Path(PATH_ENDPOINT, encoded = true) endpoint: String,
+        @Path(PATH_CATEGORY, encoded = true) category: String,
+        @Query(QUERY_API_KEY) key: String,
+        @Query(QUERY_LANGUAGE) language: String,
+        @Query(QUERY_PAGE) page: Int
+
+    ): Call<ResponseMoviesList>
 
 }
