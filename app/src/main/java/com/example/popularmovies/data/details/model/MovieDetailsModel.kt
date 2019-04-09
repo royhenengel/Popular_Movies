33ccfdb9c3bc6d02c8c3bcb2edd5d8ce24353431
Chen Engel <Henengel@gmail.com>
2019-04-09 18:09:28 +0300

    Return model object
    # Create model object
    # Map response to model object
    # Return model to view model

---
+++ app/src/main/java/com/example/popularmovies/data/details/model/MovieDetailsModel.kt
@@ -0,0 +1,22 @@
+package com.example.popularmovies.data.details.model
+
+import java.util.*
+
+
+data class MovieDetailsModel(
+
+        val id: Int,
+
+        val title: String?,
+
+        val overview: String?,
+
+        val releaseDate: Date?,
+
+        val score: Double?,
+
+        val length: Int?,
+
+        val thumbnailPath: String?
+
+)
---
+++ app/src/main/java/com/example/popularmovies/data/main/source/remote/mapper/MovieDetailsResponseToModelMapper.kt
@@ -0,0 +1,26 @@
+package com.example.popularmovies.data.main.source.remote.mapper
+
+import androidx.arch.core.util.Function
+import com.example.popularmovies.api.details.model.ResponseMovieDetails
+import com.example.popularmovies.data.details.model.MovieDetailsModel
+import com.example.popularmovies.util.dateFromString
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MovieDetailsResponseToModelMapper @Inject constructor() : Function<ResponseMovieDetails, MovieDetailsModel> {
+
+    override fun apply(movieDetailsResponse: ResponseMovieDetails): MovieDetailsModel {
+
+        return MovieDetailsModel(
+                id = movieDetailsResponse.id!!,
+                title = movieDetailsResponse.title,
+                overview = movieDetailsResponse.overview,
+                releaseDate = dateFromString(dateString = movieDetailsResponse.releaseDate),
+                score = movieDetailsResponse.voteAverage,
+                length = movieDetailsResponse.runtime,
+                thumbnailPath = movieDetailsResponse.backdropPath
+        )
+    }
+
+}
