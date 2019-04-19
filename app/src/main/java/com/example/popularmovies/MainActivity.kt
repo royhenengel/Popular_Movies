5cec5f18e081d760ca3189e4597fc76db90b2997
Chen Engel <Henengel@gmail.com>
2019-04-19 14:48:59 +0300

    Implement toolbar menu xml files
    # Add toolbar icon drawables
    # Implement menu xml files

--- app/src/main/java/com/example/popularmovies/MainActivity.kt
+++ app/src/main/java/com/example/popularmovies/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_main_movies, menu)
         return true
     }
 
